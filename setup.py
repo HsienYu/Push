4d3268587312a545650f39536aa000af52319bb8
H.Y.C <chenghsienyu@aim.com>
2019-01-23 19:46:18 +0800

    create folder and setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="push4pi",
+    version="1.0.0",
+    description="",
+    url="",
+    keywords="",
+    author="",
+    author_email="",
+    license="",
+    packages=["push4pi"],
+    install_requires=[
+        "RPi.GPIO"
+    ],
+)
