18db6e8cc5a5c14f027b32d995055e99ad976b59
H.Y.C <chenghsienyu@aim.com>
2019-02-24 22:06:06 +0800

    add requests
    add requesting get x y btn

--- push4pi/btn.py
+++ push4pi/btn.py
@@ -1,17 +1,18 @@
 import RPi.GPIO as GPIO
 import time
+import requests
 
 GPIO.setmode(GPIO.BCM)
 
 rows = [17,27,22,5,6,13]
 cols = [19,26,14,15,23,24]
 keys = [
-        ["a1","a2","a3","a4","a5","a6"],
-        ["b1","b2","b3","b4","b5","b6"],
-        ["c1","c2","c3","c4","c5","c6"],
-        ["d1","d2","d3","d4","d5","d6"],
-        ["e1","e2","e3","e4","e5","e6"],
-        ["f1","f2","f3","f4","f5","f6"]
+        ["1-6","1-5","1-4","1-3","1-2","1-1"],
+        ["2-6","2-5","2-4","2-3","2-2","2-1"],
+        ["3-1","3-2","3-3","3-4","3-5","3-6"],
+        ["4-6","4-5","4-4","4-3","4-2","4-1"],
+        ["5-1","5-2","5-3","5-4","5-5","5-6"],
+        ["6-6","6-5","6-4","6-3","6-2","6-1"]
         ]
 
 for row_pin in rows:
@@ -30,8 +31,23 @@ def get_key():
         GPIO.output(col_pin, 0)
     return key
 
-while True:
-    key = get_key()
-    if key :
-        print(key)
-    time.sleep(0.2)
+try:
+    while True:
+        key = get_key()
+        if key :
+            #print key
+            position = key.split('-')
+            x = position[0]
+            y = position[1]
+            print position
+            btn_params = { 'x' = x , 'y' = y}
+            r = requests.get('http://localhost:3000/api/button/', params = btn_params)
+            print r.url
+            print r.status_code
+            time.sleep(0.2)
+
+except KeyboardInterrupt:
+    print "done"
+
+finally:
+    GPIO.cleanup()
